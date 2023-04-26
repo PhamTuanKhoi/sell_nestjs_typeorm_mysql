32fdd700ea6964c1ab7f7ea6f12672d5bdf282a9
huynhanhpham <huynhanhpham734@gmail.com>
2023-04-26 14:07:21 +0700

    import dto validate

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,31 @@
-export class CreateUserDto {}
+import {
+  IsString,
+  IsOptional,
+  IsNotEmpty,
+  MinLength,
+  Length,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsOptional()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(4, 20)
+  username: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(4)
+  password: string;
+
+  @IsOptional()
+  @IsString()
+  avatar: string;
+
+  @IsOptional()
+  @IsString()
+  email: string;
+}
