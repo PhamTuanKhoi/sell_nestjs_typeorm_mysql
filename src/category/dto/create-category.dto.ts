f62b2fca3657884383a804d752183093103101cd
huynhanhpham <huynhanhpham734@gmail.com>
2023-04-27 17:24:59 +0700

    dto category

--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,1 +1,15 @@
-export class CreateCategoryDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+
+export class CreateCategoryDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @Length(4, 30)
+  name: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  user: string;
+}
